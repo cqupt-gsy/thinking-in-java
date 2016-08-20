c8b75cfa46b18b092f5a6902a03bb8c8ffa2034f
cqupt-gsy <gaosy0106@gmail.com>
2016-08-20 22:14:35 +0530

    testing the default value of integer and char

--- src/main/java/gsy/chapter/two/Exercise_01.java
+++ src/main/java/gsy/chapter/two/Exercise_01.java
@@ -1,4 +1,14 @@
 package gsy.chapter.two;
 
 public class Exercise_01 {
+    private int index;
+    private char character;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
 }
--- src/test/java/gsy/chapter/two/Exercise_01Tests.java
+++ src/test/java/gsy/chapter/two/Exercise_01Tests.java
@@ -1,6 +1,9 @@
 package gsy.chapter.two;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class Exercise_01Tests {
     private Exercise_01 exercise01;
@@ -8,6 +11,17 @@ public class Exercise_01Tests {
     @Before
     public void setUp() throws Exception {
         exercise01 = new Exercise_01();
-
     }
+
+    @Test
+    public void shouldReturnZeroWhenGetTheIntergeDefaultValue() {
+        assertEquals(exercise01.getIndex(), 0);
+    }
+
+    @Test
+    public void shouldReturnEmptyCharWhenGetTheCharDefaultValue() {
+        assertEquals(exercise01.getCharacter(), 0);
+    }
+
+
 }
