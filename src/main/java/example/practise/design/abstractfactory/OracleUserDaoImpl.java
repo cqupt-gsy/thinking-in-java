6d5ef16f6b026f6fb1f2e77aeb1a42899d3cc491
cqupt-gsy <gaosy0106@gmail.com>
2016-11-07 22:43:28 +0800

    [Mentu][Abstract-Factory] refactor constant strings

--- src/main/java/example/practise/design/abstractfactory/OracleUserDaoImpl.java
+++ src/main/java/example/practise/design/abstractfactory/OracleUserDaoImpl.java
@@ -5,21 +5,21 @@ import static example.practise.design.constant.ConstantStrings.*;
 
 public class OracleUserDaoImpl implements UserDao {
     public String insertUser(String name) {
-        return ORACLE + INSERT_USER + COLON + name;
+        return ORACLE + INSERT + USER + COLON + name;
     }
 
     @Override
     public String deleteUser(String name) {
-        return ORACLE + DELETE_USER + COLON  + name;
+        return ORACLE + DELETE + USER + COLON  + name;
     }
 
     @Override
     public String updateUser(String name) {
-        return ORACLE + UPDATE_USER + COLON  + name;
+        return ORACLE + UPDATE + USER + COLON  + name;
     }
 
     @Override
     public String quertUser(String name) {
-        return ORACLE + QUERY_USER + COLON + name;
+        return ORACLE + QUERY + USER + COLON + name;
     }
 }
--- src/main/java/example/practise/design/constant/ConstantStrings.java
+++ src/main/java/example/practise/design/constant/ConstantStrings.java
@@ -6,10 +6,12 @@ public class ConstantStrings {
     public static final String MYSQL = "Mysql ";
     public static final String SQLSERVER = "Sql Server ";
 
-    public static final String INSERT_USER = "Insert User";
-    public static final String DELETE_USER = "Delete User";
-    public static final String UPDATE_USER = "Update User";
-    public static final String QUERY_USER = "Query User";
+    public static final String INSERT = "Insert ";
+    public static final String DELETE = "Delete ";
+    public static final String UPDATE = "Update ";
+    public static final String QUERY = "Query ";
+
+    public static final String USER = "User";
 
     public static final String COLON = ": ";
 }
