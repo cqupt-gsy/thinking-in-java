78ee91aba08aec0657521a49d02b722e35e192f3
cqupt-gsy <gaosy0106@gmail.com>
2016-12-15 21:21:40 +0800

    [Mentu][Iterator] finish iterator pattern

--- src/main/java/example/practise/design/decorator/DecoratorOutputOperator.java
+++ src/main/java/example/practise/design/decorator/DecoratorOutputOperator.java
@@ -1,6 +1,6 @@
 package example.practise.design.decorator;
 
-//Todo: summarizing decorator, proxy, strategy，facade, builder， observer， state， adapter
+//Todo: summarizing decorator, proxy, strategy，facade, builder， observer， state， adapter, iterator
 
 public interface DecoratorOutputOperator {
     String output();
