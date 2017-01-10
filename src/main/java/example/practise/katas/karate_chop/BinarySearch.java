669a7fd4300fc52b2d0071a9fbf084082aad8a78
cqupt-gsy <gaosy0106@gmail.com>
2017-01-10 22:21:59 +0800

    [Mentu][FirstMethod] implement binary search through loop

--- src/main/java/example/practise/katas/karate_chop/BinarySearch.java
+++ src/main/java/example/practise/katas/karate_chop/BinarySearch.java
@@ -1,9 +1,36 @@
 package example.practise.katas.karate_chop;
 
 import java.util.List;
+import java.util.function.Predicate;
 
-public class BinarySearch {
-    public Integer chop(Integer element, List<Integer> dataList) {
+class BinarySearch {
+
+    private static final int DEFAULT_RESULT = -1;
+
+    Integer loopChop(Integer element, List<Integer> dataList) {
+        if (dataList != null) {
+            int beginIndices = 0;
+            int endIndices = dataList.size();
+            while (beginIndices != endIndices) {
+                int midIndices = (beginIndices + endIndices) / 2;
+                if (createCompareRule(dataList.get(midIndices)).test(element)) {
+                    endIndices = midIndices;
+                } else if (createCompareRule(element).test(dataList.get(midIndices))) {
+                    beginIndices = midIndices;
+                    ++beginIndices;
+                } else {
+                    return midIndices;
+                }
+            }
+        }
+        return DEFAULT_RESULT;
+    }
+
+    public Integer recursiveChop(Integer element, List<Integer> dataList) {
         return null;
+    }
+
+    private Predicate<Integer> createCompareRule(Integer origin) {
+        return targets -> targets < origin;
     }
 }
--- src/test/java/example/practise/katas/karate_chop/BinarySearchTest.java
+++ src/test/java/example/practise/katas/karate_chop/BinarySearchTest.java
@@ -1,6 +1,8 @@
 /*
 1.As you’re coding each algorithm, keep a note of the kinds of error you encounter.
 (that is, do you learn from experience in one technique when it comes to coding with a different technique?)
+--第一次实现时产生了死循环，原因是在找到了元素后没有及时返回，因此没有退出循环
+--打开查找不存在的数据的奇数列表测试用例后再次产生死循环，产生原因是在与中间数比较后，需要把下次比较的索引往前移动一个位置
 
 
 
@@ -9,11 +11,13 @@ Which is the most likely to make it in to production code?
 Which was the most fun to write?
 Which was the hardest to get working?
 And for all these questions, ask yourself “why?”.
+--java8 lambda 表达式，理解java8的函数式编程以及减少编码
 
 
 
 3.How did you go about coming up with approaches four and five?
 What techniques did you use to fire those “off the wall” neurons?
+--loop
 
 
 
@@ -31,72 +35,136 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class BinarySearchTest {
 
     @Test
-    public void shouldReturnMinusOneWhenArrayIsNullOrNotFindTheDataInArray() {
+    public void shouldReturnMinusOneWhenArrayIsNullOrNotFindTheDataInArrayWithLoopChop() {
         //Given
         BinarySearch binarySearch = new BinarySearch();
         List<Integer> oneElementList = createOneElementList();
 
         //Then
-        assertThat(binarySearch.chop(3, createEmptyList())).isEqualTo(-1);
-        assertThat(binarySearch.chop(3, oneElementList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(1, oneElementList)).isEqualTo(0);
+        assertThat(binarySearch.loopChop(3, createEmptyList())).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(3, oneElementList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(1, oneElementList)).isEqualTo(0);
     }
 
-    @Ignore
     @Test
-    public void shouldReturnIndicesOfElementInOddList() {
+    public void shouldReturnIndicesOfElementInOddListWithLoopChop() {
         //Given
         BinarySearch binarySearch = new BinarySearch();
         List<Integer> oddElementsList = createOddElementsList();
 
         //Then
-        assertThat(binarySearch.chop(1, oddElementsList)).isEqualTo(0);
-        assertThat(binarySearch.chop(3, oddElementsList)).isEqualTo(1);
-        assertThat(binarySearch.chop(5, oddElementsList)).isEqualTo(2);
+        assertThat(binarySearch.loopChop(1, oddElementsList)).isEqualTo(0);
+        assertThat(binarySearch.loopChop(3, oddElementsList)).isEqualTo(1);
+        assertThat(binarySearch.loopChop(5, oddElementsList)).isEqualTo(2);
     }
 
-    @Ignore
     @Test
-    public void shouldReturnMinusOneWhenElementCantFindInOddList() {
+    public void shouldReturnMinusOneWhenElementCantFindInOddListWithLoopChop() {
         //Given
         BinarySearch binarySearch = new BinarySearch();
         List<Integer> oddElementsList = createOddElementsList();
 
         //Then
-        assertThat(binarySearch.chop(0, oddElementsList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(2, oddElementsList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(4, oddElementsList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(6, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(0, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(2, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(4, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(6, oddElementsList)).isEqualTo(-1);
     }
 
-
-    @Ignore
     @Test
-    public void shouldReturnIndicesOfElementInEvenList() {
+    public void shouldReturnIndicesOfElementInEvenListWithLoopChop() {
         //Given
         BinarySearch binarySearch = new BinarySearch();
         List<Integer> evenElementsList = createEvenElementsList();
 
         //Then
-        assertThat(binarySearch.chop(1, evenElementsList)).isEqualTo(0);
-        assertThat(binarySearch.chop(3, evenElementsList)).isEqualTo(1);
-        assertThat(binarySearch.chop(5, evenElementsList)).isEqualTo(2);
-        assertThat(binarySearch.chop(7, evenElementsList)).isEqualTo(3);
+        assertThat(binarySearch.loopChop(1, evenElementsList)).isEqualTo(0);
+        assertThat(binarySearch.loopChop(3, evenElementsList)).isEqualTo(1);
+        assertThat(binarySearch.loopChop(5, evenElementsList)).isEqualTo(2);
+        assertThat(binarySearch.loopChop(7, evenElementsList)).isEqualTo(3);
     }
 
-    @Ignore
     @Test
-    public void shouldReturnMinusOneWhenElementCantFindInEvenList() {
+    public void shouldReturnMinusOneWhenElementCantFindInEvenListWithLoopChop() {
         //Given
         BinarySearch binarySearch = new BinarySearch();
         List<Integer> oddElementsList = createOddElementsList();
 
         //Then
-        assertThat(binarySearch.chop(0, oddElementsList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(2, oddElementsList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(4, oddElementsList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(6, oddElementsList)).isEqualTo(-1);
-        assertThat(binarySearch.chop(8, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(0, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(2, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(4, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(6, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.loopChop(8, oddElementsList)).isEqualTo(-1);
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMinusOneWhenArrayIsNullOrNotFindTheDataInArrayWithRecursiveChop() {
+        //Given
+        BinarySearch binarySearch = new BinarySearch();
+        List<Integer> oneElementList = createOneElementList();
+
+        //Then
+        assertThat(binarySearch.recursiveChop(3, createEmptyList())).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(3, oneElementList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(1, oneElementList)).isEqualTo(0);
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnIndicesOfElementInOddListWithRecursiveChop() {
+        //Given
+        BinarySearch binarySearch = new BinarySearch();
+        List<Integer> oddElementsList = createOddElementsList();
+
+        //Then
+        assertThat(binarySearch.recursiveChop(1, oddElementsList)).isEqualTo(0);
+        assertThat(binarySearch.recursiveChop(3, oddElementsList)).isEqualTo(1);
+        assertThat(binarySearch.recursiveChop(5, oddElementsList)).isEqualTo(2);
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMinusOneWhenElementCantFindInOddListWithRecursiveChop() {
+        //Given
+        BinarySearch binarySearch = new BinarySearch();
+        List<Integer> oddElementsList = createOddElementsList();
+
+        //Then
+        assertThat(binarySearch.recursiveChop(0, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(2, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(4, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(6, oddElementsList)).isEqualTo(-1);
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnIndicesOfElementInEvenListWithRecursiveChop() {
+        //Given
+        BinarySearch binarySearch = new BinarySearch();
+        List<Integer> evenElementsList = createEvenElementsList();
+
+        //Then
+        assertThat(binarySearch.recursiveChop(1, evenElementsList)).isEqualTo(0);
+        assertThat(binarySearch.recursiveChop(3, evenElementsList)).isEqualTo(1);
+        assertThat(binarySearch.recursiveChop(5, evenElementsList)).isEqualTo(2);
+        assertThat(binarySearch.recursiveChop(7, evenElementsList)).isEqualTo(3);
+    }
+
+    @Ignore
+    @Test
+    public void shouldReturnMinusOneWhenElementCantFindInEvenListWithRecursiveChop() {
+        //Given
+        BinarySearch binarySearch = new BinarySearch();
+        List<Integer> oddElementsList = createOddElementsList();
+
+        //Then
+        assertThat(binarySearch.recursiveChop(0, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(2, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(4, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(6, oddElementsList)).isEqualTo(-1);
+        assertThat(binarySearch.recursiveChop(8, oddElementsList)).isEqualTo(-1);
     }
 
     private List<Integer> createEmptyList() {
