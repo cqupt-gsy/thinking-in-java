28c1219de812eb5d38d74e950ea9995f449862db
cqupt-gsy <gaosy0106@gmail.com>
2018-06-27 20:57:56 +0800

    [Shiyi] add reverse integer solution

---
+++ src/main/java/programming/practise/leetcodes/ReverseInteger_07.java
@@ -0,0 +1,22 @@
+package programming.practise.leetcodes;
+
+public class ReverseInteger_07 {
+    public int reverse(int x) {
+        int[] separatedInteger = new int[10];
+        int currentIndex = 0;
+        while (x != 0) {
+            separatedInteger[currentIndex] = x % 10;
+            x = x / 10;
+            currentIndex++;
+        }
+        int result = 0;
+        for (int index = 0, reverseIntLength = currentIndex - 1; reverseIntLength >= 0 ; reverseIntLength--, index++) {
+            if (separatedInteger[index] == 0 && result == 0) {
+                currentIndex--;
+                continue;
+            }
+            result += separatedInteger[index] * Math.pow(10, reverseIntLength);
+        }
+        return result == Integer.MAX_VALUE || result == Integer.MIN_VALUE ? 0 : result;
+    }
+}
