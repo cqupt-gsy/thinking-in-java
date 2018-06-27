1d718b7c2054678ea2bb0834c0ab1b4ab9be708a
cqupt-gsy <gaosy0106@gmail.com>
2018-06-27 16:31:55 +0800

    [Shiyi] add zig zag solution

---
+++ src/main/java/programming/practise/leetcodes/ZigZagConversion_06.java
@@ -0,0 +1,32 @@
+package programming.practise.leetcodes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigZagConversion_06 {
+
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        List<StringBuilder> zigZagStringBuilders = new ArrayList<>();
+        for (int index = 0; index < Math.min(s.length(), numRows); index++) {
+            zigZagStringBuilders.add(new StringBuilder());
+        }
+
+        boolean goingDown = false;
+        int currentRowIndex = 0;
+
+        for (int index = 0; index < s.length(); index++) {
+            zigZagStringBuilders.get(currentRowIndex).append(s.charAt(index));
+            if (currentRowIndex == 0 || currentRowIndex == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currentRowIndex += goingDown ? 1 : -1;
+        }
+
+        StringBuilder zigZagString = new StringBuilder();
+        zigZagStringBuilders.forEach(zigZagStringBuilder -> zigZagString.append(zigZagStringBuilder.toString()));
+        return zigZagString.toString();
+    }
+}
