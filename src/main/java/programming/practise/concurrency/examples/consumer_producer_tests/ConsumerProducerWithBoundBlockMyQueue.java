b4e7346d31d664dd01d219fe120eedbf08fb0df7
sygao <sygao@gaoshiyis-MacBook-Pro.local>
2019-08-18 22:52:22 +0800

    [Shiyi] update setting

--- src/main/java/programming/practise/concurrency/examples/consumer_producer_tests/ConsumerProducerWithBoundBlockMyQueue.java
+++ src/main/java/programming/practise/concurrency/examples/consumer_producer_tests/ConsumerProducerWithBoundBlockMyQueue.java
@@ -8,27 +8,29 @@ import java.util.concurrent.CountDownLatch;
 
 public class ConsumerProducerWithBoundBlockMyQueue {
 
-    static final int PRODUCER_NUMBER = 3;
-    static final int CONSUMER_NUMBER = 2;
+  static final int PRODUCER_NUMBER = 3;
+  static final int CONSUMER_NUMBER = 2;
 
-    public static void main(String args[]) {
-        CountDownLatch countDownLatch = new CountDownLatch(PRODUCER_NUMBER);
-//        BoundBlockQueueWithLock queue = new BoundBlockQueueWithLock(10);
-        BoundBlockQueueWithSync queue = new BoundBlockQueueWithSync(10);
-        for (int i = 1; i <= PRODUCER_NUMBER; i++) {
-            new Thread(new ProducerMyQueueBlock(queue, countDownLatch), "P"+i).start();
-        }
-        new Thread(() -> {
-            try {
+  public static void main(String args[]) {
+    CountDownLatch countDownLatch = new CountDownLatch(PRODUCER_NUMBER);
+    //        BoundBlockQueueWithLock queue = new BoundBlockQueueWithLock(10);
+    BoundBlockQueueWithSync queue = new BoundBlockQueueWithSync(10);
+    for (int i = 1; i <= PRODUCER_NUMBER; i++) {
+      new Thread(new ProducerMyQueueBlock(queue, countDownLatch), "P" + i).start();
+    }
+    new Thread(
+            () -> {
+              try {
                 countDownLatch.await();
                 queue.put("END");
                 System.out.println("ADD END TASK");
-            } catch (InterruptedException e) {
+              } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
-        }).start();
-        for (int i = 1; i <= CONSUMER_NUMBER; i++) {
-            new Thread(new ConsumerMyQueueBlock(queue), "C"+i).start();
-        }
+              }
+            })
+        .start();
+    for (int i = 1; i <= CONSUMER_NUMBER; i++) {
+      new Thread(new ConsumerMyQueueBlock(queue), "C" + i).start();
     }
+  }
 }
